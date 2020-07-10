7043056149d77d2420acf51de7301729cbc19252
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-10 15:01:00 +0530

    Initial Commit

---
+++ initialise_db.py
@@ -0,0 +1,14 @@
+from openpyxl import load_workbook
+from openpyxl.styles import Border, Side, Alignment, colors, PatternFill, Font, Fill
+from openpyxl.styles.colors import Color
+from openpyxl.utils import get_column_letter
+import pandas as pd
+from filepaths import *
+
+
+class InitialiseDb:
+    def __init__(self):
+        """
+        Gets the names of the input stocks.
+        """
+        pass
