a29b91ff3a59a5abb29608d79466e1248f664a04
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-10 16:57:52 +0530

    Added scraping functionality.

--- store_data.py
+++ store_data.py
@@ -10,35 +10,54 @@ from datetime import date
 
 
 class StoreData()
-   def __init__(self, parameters):
+   def __init__(self):
         """
         Initialises the various dataframes, stylistic variables and data
         structures for the program! Accepts the parameters that are to be
         stored.
+        As reports are released for the prev day then the current day is
+        considered to be yesterday.
         """
-        self.date_today = date.today()
-        self.d1 = self.date_today.strftime("%d-%m-%Y")
+        date_today = date.today() - timedelta(days=1)
+        self.url_date = date_today.strftime("%d%m%Y")
+        self.base_url = 'https://archives.nseindia.com/products/content/'
         self.border = Border(left=Side(border_style='thin', color='000000'),
                              right=Side(border_style='thin', color='000000'),
                              top=Side(border_style='thin', color='000000'),
                              bottom=Side(border_style='thin', color='000000'))
         self.ft = Font(color='FFFFFF', bold=True, name='Times New Roman')
         self.allign_style = 'center'
+        self.parameters = ['CLOSE_PRICE', 'DELIV_PER']
         self.stored_names, self.input_names, self.cell_range, self.url = [], [], '', ''
-        self.get_url()
+        self.get_file()
+        wb = load_workbook(stored_path)
+        self.ws = wb.active
+        self.date_column()
+        os.remove(self.file_path)
 
-    def get_url(self):
+    def get_file(self):
         """
         Retrieves the url for the file that is to be scraped.
         """
-        pass
+        self.url = self.base_url + 'sec_bhavdata_full_{}.csv'.format(self.url_date)
+        # print(self.url)
+        values = requests.get(self.url)
+        self.file_path = 'sec_bhavdata_full_{}.csv'.format(self.url_date)
+        fhand = open(self.file_path, 'wb')
+        fhand.write(values.content)
+        fhand.close()
+        self.retrieve_data()
 
-    def scrape_data(self):
+    def retrieve_data(self):
         """
         Scrapes the data retrieved, adds it to the data frames and parses the
         data.
         """
-        pass
+        raw_input_df = pd.read_csv(self.file_path, sep=r'\s*,\s*', engine='python')
+        input_df = raw_input_df[raw_input_df["SERIES"] == 'EQ']
+        self.input_names = input_df['SYMBOL']
+        stored_data = pd.read_excel(stored_path)
+        self.stored_names = stored_data['STOCKS']
 
     def date_column(self):
         """
