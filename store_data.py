d50046d801e2c87e45c2f6d057329ee18f3e2b51
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-07-10 15:36:15 +0530

    Added functions and the docs strings.

--- store_data.py
+++ store_data.py
@@ -5,3 +5,53 @@ from openpyxl.utils import get_column_letter
 import pandas as pd
 from filepaths import *
 import requests
+
+
+class StoreData()
+   def __init__(self, parameters):
+        """
+        Initialises the various dataframes, stylistic variables and data
+        structures for the program! Accepts the parameters that are to be
+        stored.
+        """
+        pass
+
+    def get_url(self):
+        """
+        Retrieves the url for the file that is to be scraped.
+        """
+        pass
+
+    def scrape_data(self):
+        """
+        Scrapes the data retrieved, adds it to the data frames and parses the
+        data.
+        """
+        pass
+
+    def date_column(self):
+        """
+        Calculates which column is to be used for the day. Fills in the date
+        that is to be used for the present day. Merges the cell, stylises it,
+
+        """
+        pass
+
+    def stylise_cells(self):
+        """
+        Stylises cells within a range with the border, allignment etc.
+        """
+        pass
+
+    def enter_data(self):
+        """
+        Enters the data in the excel sheet based on the names that are already
+        stored and those that are new
+        """
+        pass
+
+    def arrange_rows(self):
+        """
+        Sorts all the names within the different rows.
+        """
+        pass
